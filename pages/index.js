f239406b7aabfb438c19ad7b852859040eab4f8f
ImaneIK <imaneik0910@gmail.com>
2023-09-14 19:36:32 +0100

    responsivness and additional sections

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import Home from '../components/Home';
+import Footer from '../components/Footer';
+import Projects from '@/components/Projects';
 
 export default function Root() {
   return (
-    <Home/>
+    <div>
+      <Home/>
+      <Projects/>
+      <Footer/>
+    </div>
+    
   );
 }
